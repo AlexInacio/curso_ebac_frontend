f4648dfe08a973caf3221fc627343d9184ae1dd9
Alex Inacio <inacio.alesilva@gmail.com>
2024-12-04 23:24:18 -0300

    Feature: Adicao exercicio Calcular Area Js

---
+++ exercicio_js/CalculaArea.js
@@ -0,0 +1,11 @@
+
+function calculaArea(base, altura){
+    let area = base * altura
+
+    console.log(`O resultado do calculo de area é ${area}m`)
+    return area
+}
+
+
+//forneca base e altura em metros, ex: 2
+calculaArea(2,2)
